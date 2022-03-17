e8e90997bf9e8738b11f11b68624e5b58ba7832e
David <madunts.d@gmail.com>
2022-03-17 19:17:02 +0500

    Add UnsplashApi with requests

---
+++ app/src/main/java/com/example/composepagingunsplash/data/remote/UnsplashApi.kt
@@ -0,0 +1,23 @@
+package com.example.composepagingunsplash.data.remote
+
+import com.example.composepagingunsplash.BuildConfig
+import com.example.composepagingunsplash.model.UnsplashImage
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface UnsplashApi {
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/photos")
+    suspend fun getAllImages(
+        @Query("page") pageNumber: Int,
+        @Query("per_page") perPage: Int
+    ): List<UnsplashImage>
+
+    @Headers("Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/search/photos ")
+    suspend fun searchImage(
+        @Query("page") pageNumber: Int,
+        @Query("per_page") perPage: Int
+    ): List<UnsplashImage>
+}
