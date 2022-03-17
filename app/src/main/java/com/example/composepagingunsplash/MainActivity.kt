dcb94b2510c2e3bb155208b9794e3ec79ddaaf72
David <madunts.d@gmail.com>
2022-03-17 18:46:55 +0500

    Project setup

---
+++ app/src/main/java/com/example/composepagingunsplash/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.composepagingunsplash
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
